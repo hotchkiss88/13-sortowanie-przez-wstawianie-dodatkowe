7adde37a8f2fdb588800eaaee46f3beff3554564
Grzegorz Sowinski <88460670+hotchkiss88@users.noreply.github.com>
2021-09-15 12:16:45 +0000

    format

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 let table = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1];
 
 function sorting(x) {
-    let storage = 0;
-    for (i = 0; i < x.length; i++) {
+  let storage = 0; //przechowuje najmniejsza wartość
+  for (i = 0; i < x.length; i++) {
 
-        storage = x[i];
+    storage = x[i]; //przypisanie indeksu wartości
 
-        let j = i - 1;
+    let j = i - 1;
 
-        while (j >= 0 && x[j] > storage) {
-            x[j + 1] = x[j];
+    while (j >= 0 && x[j] > storage) {//porównanie dwóch wartości i indexów
+      x[j + 1] = x[j]; //zamiana miejscami
 
-            j = j - 1;
-        }
-        x[j + 1] = storage;
+      j = j - 1;
     }
-    return storage;
+    x[j + 1] = storage; //przypisanie nowego elementu
+  }
+  return storage;
 }
 
 sorting(table);
